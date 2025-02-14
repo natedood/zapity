751853ac3d21702c4ef8199107b5e105dd535fa8
natedood <167116278+natedood@users.noreply.github.com>
2025-02-14 15:22:23 -0600

    Update db_connect.php
    security update A78

--- db_connect.php
+++ db_connect.php
@@ -3,7 +3,7 @@
 // Connect to the MySQL database
 $servername = "localhost";
 $username = "zapity";
-$password = "Abig@il1978";
+$password = "xxxxxxxxxx";
 $dbname = "zapity";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
