f27047ccede9c80c08d383747c67dad5763df493
Nate <natedood@gmail.com>
2025-02-20 11:51:25 -0600

    call screen phone number history lookup initial commit

---
+++ callscreen_getcalls.php
@@ -0,0 +1,19 @@
+<?php
+// Enable error reporting for debugging (remove or disable for production!)
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include 'db_connect.php';
+
+// Decode the JSON data from the AJAX POST request
+$data = json_decode($_POST['data'], true);
+if (!$data) {
+    die("Error decoding JSON: " . json_last_error_msg());
+}
+
+// Close the database connection
+$conn->close();
+
+// Return a success response
+echo json_encode(['status' => 'success']);
+?>
