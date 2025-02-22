726d3fd69c3be66db2d42eb46775dedbf55c6f32
Nate <natedood@gmail.com>
2025-02-22 15:05:38 -0600

    call screen and todo changes - limiting to unique numbers on todo, added ability to make todo items done when redirected to callscreen from todo, marking items as done.

--- callscreen.php
+++ callscreen.php
@@ -19,6 +19,28 @@ $followup_date = $data['followup_date'];
 $call_notes    = $data['call_notes'];
 $call_origin   = $data['call_origin'];
 
+
+// Check if clearTodo is set and equals 1
+if (isset($data['clearTodo']) && $data['clearTodo'] == 1) {
+    // server is in UTC, so subtract 6 hours to get the correct date in CST
+    // todo: timzone should be set in php.ini
+    $today = date('Y-m-d', strtotime('-6 hours'));
+    $stmt = $conn->prepare("UPDATE todos t
+                            JOIN calls c ON t.link_id = c.id
+                            SET t.status = 1
+                            WHERE t.due_datetime = ? 
+                            AND c.phone_number = ?");
+    if (!$stmt) {
+        die("Prepare failed (update todos): " . $conn->error);
+    }
+    $stmt->bind_param("ss", $today, $phone_number);
+    if (!$stmt->execute()) {
+        die("Execute failed (update todos): " . $stmt->error);
+    }
+    $stmt->close();
+}
+
+
 // Insert into the calls table, including the call_origin field
 $call_datetime = date('Y-m-d H:i:s');
 $stmt = $conn->prepare("INSERT INTO calls (phone_number, call_datetime, call_origin, notes, caller_id_name) VALUES (?, ?, ?, ?,?)");
